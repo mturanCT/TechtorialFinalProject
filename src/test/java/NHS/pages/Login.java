e8381f8c7d9e7d3b32fd71e6d36cfdf177037262
Mustafa <mustafaturan28@gmail.com>
2023-02-13 12:26:31 -0500

    Project

--- src/test/java/NHS/pages/Login.java
+++ src/test/java/NHS/pages/Login.java
@@ -1,5 +1,6 @@
 package NHS.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
